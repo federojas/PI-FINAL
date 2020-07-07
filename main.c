465fd5ef459a7a293f807db2e17f4e537c5e669a
robrodriguez99 <64867075+robrodriguez99@users.noreply.github.com>
2020-07-07 18:02:20 -0300

    mini changes main

--- main.c
+++ main.c
@@ -8,20 +8,29 @@ enum errors {ARG_ERR = 1, NO_MEM, }; // poner en el .H
 
 int main(int argc, char const *argv[])
 {
-    if(argc != 3)
+   /* if(argc != 3)
     {
         printf("Incorrect amount of arguments.\n");
         return ARG_ERR;
-    }
+    }*/
     // initialize ADT
     // the csv files are created
     FILE *trees, *neighborhoods, *query1,*query2, *query3;
     //the files needed are opened
     trees =fopen(argv[1],"r");
     neighborhoods=fopen(argv[2],"r");
+    if(trees==NULL||neighborhoods== NULL)
+    {
+        printf("parameter error\n");
+        return ARG_ERR;
+    }
     query1=fopen("query1.csv","w");
     query2=fopen("query2.csv","w");
     query3=fopen("query3.csv","w");
 
+    char asd[100];
+    //fgets(asd,100,trees);       
+    fgets(asd,100,neighborhoods); 
+    printf("%s\n",asd);      
     return OK;
 }
