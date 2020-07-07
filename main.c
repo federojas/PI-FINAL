8876264e25c1702e23031411c4fd24b8dc28bf94
robrodriguez99 <64867075+robrodriguez99@users.noreply.github.com>
2020-07-07 14:41:33 -0300

    Update main.c
    se agregan los files para trabajar

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
+#include <string.h>
+//we should include our adt.h later
 #define OK 0
 enum errors {arg_Err = 1, };
 
@@ -11,5 +13,15 @@ int main(int argc, char const *argv[])
         printf("Incorrect amount of arguments.\n");
         return arg_Err;
     }
+    // here we should initialize our adt 
+    // we create the csv files
+    FILE *trees, *neighborhoods, *query1,*query2, *query3;
+    //we open the files to work 
+    trees =fopen(argv[1],"r");
+    neighborhoods=fopen(argv[2],"r");
+    query1=fopen("query1.csv","w");
+    query2=fopen("query2.csv","w");
+    query3=fopen("query3.csv","w");
+
     return OK;
 }
