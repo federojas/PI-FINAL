cc568122787a711331f7bdc03fb707b956b83e4d
federojas <frojas@itba.edu.ar>
2020-07-08 18:36:19 -0300

    Separacion de TADS

--- hoodADT.c
+++ hoodADT.c
@@ -1,4 +1,5 @@
 #include "hoodADT.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
@@ -16,6 +17,17 @@ typedef struct hood {
 } hood;
 
 typedef struct hoodCDT {
-    hood * firstHoodQty;          // neighborhoos in descending order by qty
-    hood * firstHoodHab;          // neighborhoos in descending order by qty/habitants
+    hood * firstHoodQty;          // neighborhoods in descending order by qty, alphabetical order used to resolve draws
+    hood * firstHoodHab;          // neighborhoods in descending order by qty/habitants, alphabetical order used to resolve draws
+}
+
+typedef hoodCDT * hoodADT;
+
+hoodADT newHood() {
+    hoodADT hood;
+    if((hood = calloc(1, sizeof(hoodCDT))) == NULL) {
+        printf("No memory available\n");
+        return NO_MEM;
+    }
+    return hood;
 }
--- treesADT.c
+++ treesADT.c
@@ -5,8 +5,8 @@
 
 typedef struct tTree {
     char * common_name; // scientific name
-    size_t diameter;
-    size_t qty;
+    size_t diameterSum; // sum of all database species diameters
+    size_t qty; //amount of species specimens on data base
     size_t diameterMean; // average diameter of tree species
 } tTree;
 
@@ -16,7 +16,7 @@ typedef struct tree {
 } tree;
 
 typedef struct treesCDT {
-    tree * firstTreeByDiameter;   // trees in descending order by average species diameter
+    tree * firstTree;   // trees in descending order by average species diameter, alphabetical order used to resolve draws
 } treesCDT;
 
 typedef struct treesCDT * treesADT;
@@ -25,22 +25,9 @@ treesADT newTree() {
     treesADT tree;
     if((tree = calloc(1, sizeof(treesCDT))) == NULL) {
         printf("No memory available\n");
-        return no_mem;
+        return NO_MEM;
     }
     return tree;
 }
-
-<<<<<<< HEAD
-void freeTree
-static Neighborhood *addRecHood(Neighborhood first, )
-void addHood(Neighborhood hood, treesADT tree)
-{
-
-    Neighborhood *aux=NULL;// an auxiliary pointer to node is created to save the newly created node's memory location
-    tree->treeQtyFirst=addRec(tree->treeQtyFirst,hood, &aux);
-}
-=======
-
->>>>>>> 1d4453e778f06029cffa42f2562d6c5d5bdc8417
 
 
