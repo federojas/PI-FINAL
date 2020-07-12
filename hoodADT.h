aba33b841e8fb77215d9643e0c3622e1a98b54f7
federojas <frojas@itba.edu.ar>
2020-07-12 14:07:22 -0300

    explicaciones funciones hood

--- hoodADT.h
+++ hoodADT.h
@@ -3,16 +3,39 @@
 #ifndef hoodADT_h
 #define hoodADT_h
 
-typedef struct hoodCDT * hoodADT; 
+typedef struct hoodCDT * hoodADT;
+
+//creates a new hoodADT
 hoodADT newHood();
+
+//adds a neighborhood to the hood vector
 int addHood (hoodADT hood, char * name, size_t habitants);
+
+//Adds the tree to the tree quantity of its neighborhood
 int addTreeHood(hoodADT hood, char * treeHood);
+
+//frees up all used memory
 void freeHood(hoodADT hood);
+
+//creates a single neighborhood list sorted by two different criteria. Criteria 1: descending order of trees / habitant. Criteria 2: descending order of amount of trees. Both criteria use aphabetical order as a secondary sort criteria.
 int hoodList (hoodADT hood);
+
+//sets the current iterator to the beginning of the list in descending order of trees / habitant
 void toBeginHoodHab(hoodADT hood);
+
+//checks if the current iterator is at the end of the list in descending order of trees / habitant
 int hasNextHoodHab(hoodADT hood);
-//hoodNode * nextHoodHab(hoodADT hood);
+
+//advances the current iterator to the next node in descending order of trees / habitant
+double nextHoodHab(hoodADT hood);
+
+//sets the current iterator to the beginning of the list in descending order of amount of trees
 void toBeginQty(hoodADT hood);
+
+//checks if the current iterator is at the end of the list in descending order of amount of trees
 int hasNextHoodQty(hoodADT hood);
+
+//advances the current iterator to the next node in descending order of amount of trees
 void nextHoodQty(hoodADT hood, int *cant, char *hoodName);
+
 #endif
--- main.c
+++ main.c
@@ -4,90 +4,85 @@
 #include <string.h>
 #include "treesADT.h"
 #include "hoodADT.h"
-//INCLUIR HOODADT.H Y TREESADT.H
 #define OK 0 // poner en el .H
 //(este es el main de buenos aires) 
 enum errors {ARG_ERR = 1, NO_MEM, }; // poner en el .H
 int main(int argc, char const *argv[]){
-    // if (argc!=3){ ESTA PARTE VA CUANDO YA ANDEN LOS DOS .H Y MANDEMOS LOS DOS CSV
-    //     printf("cantidad de argumentos incorrecta\n");
-    //     return ARG_ERR;
-    // }
+    if (argc!=3){
+        printf("cantidad de argumentos incorrecta\n");
+        return ARG_ERR;
+    }
     treesADT  tree=newTree();
     hoodADT hood=newHood();
     FILE *trees, *hoods,*query3,*query1,*query2;
     char *token;
-    query1=fopen("query1.csv","w"); //we open the file with "write" permissions in order to work 
-    query2=fopen("query2.csv","w"); //we open the file with "write" permissions in order to work 
-    query3=fopen("query3.csv","w"); //we open the file with "write" permissions in order to work 
+    query1=fopen("query1.csv","w"); //the file is opened with "write" permissions so that it can be used to work
+    query2=fopen("query2.csv","w"); //the file is opened with "write" permissions so that it can be used to work
+    query3=fopen("query3.csv","w"); //the file is opened with "write" permissions so that it can be used to work
     trees = fopen(argv[1],"r");
     hoods=fopen(argv[2],"r");
-    // char linesTrees[1024],linesHoods[1024];
-    // char treeName[30],hoodName[5];
-    // fgets(linesHoods,1024, hoods);//we skip the first line 
-    // fgets(linesTrees,1024, trees);//we skip the first line 
-    // int i;
-    // long pop;
-    // float diameter;
-    // char prueba[]="la concha de tu madre all boys";
-    // //we already know that the hoods are in the third column, the tree name in the 7th and the diameter in the 12th and
-    // while(fgets(linesHoods,1024, hoods)){
-    //     token=strtok(linesHoods,";");//we already know that the first column goes for the hood and the second for the population
-    //     strcpy(hoodName,token);
-    //     token = strtok(NULL,";");
-    //     pop = atol(token);
-    //     if(pop != 0)
-    //         addHood(hood,hoodName,pop);
+    char linesTrees[1024],linesHoods[1024];
+    char treeName[80],hoodName[5];
+    fgets(linesHoods,1024, hoods);//the first line is skipped
+    fgets(linesTrees,1024, trees);//the first line is skipped 
+    int i;
+    long pop;
+    float diameter;
+    //we already know that the hoods are in the third column, the tree name in the 7th and the diameter in the 12th
+    while(fgets(linesHoods,1024, hoods)){
+        token=strtok(linesHoods,";");//we already know that the first column goes for the hood and the second for the population
+        strcpy(hoodName,token);
+        token = strtok(NULL,";");
+        pop = atol(token);
+        if(pop != 0)
+            addHood(hood,hoodName,pop);
         
-    // }
+    }
 
-    // while(fgets(linesTrees,1024, trees))
-    // {
-    //     for(i=0,token=strtok(linesTrees,";");i<12;i++)
-    //     {
-    //         if(i==2)
-    //         {
-    //             strcpy(hoodName,token);
-    //         }
-    //         if(i==7)
-    //         {
-    //             strcpy(treeName,token);
-    //         }
-    //         if(i==11)
-    //         {
-    //             diameter=atoi(token);
+    while(fgets(linesTrees,1024, trees))
+    {
+        for(i=0,token=strtok(linesTrees,";");i<12;i++)
+        {
+            if(i==2)
+            {
+                strcpy(hoodName,token);
+            }
+            if(i==7)
+            {
+                strcpy(treeName,token);
+            }
+            if(i==11)
+            {
+                diameter=atoi(token);
                 
-    //         }
-    //         token=strtok(NULL,";");
-    //     }
-    //     if(diameter!=0)
-    //         addTree(tree,treeName,diameter);
-    //     addTreeHood(hood, hoodName);
-    // }
-    //     hoodList(hood);
-    //     vecToList(tree);
-    //     toBegin(tree);
-    //     toBeginHoodHab(hood);
-    //     toBeginQty(hood);
-    //     int qty;
-    //     //fprintf(query1,"%s\n",prueba);
+            }
+            token=strtok(NULL,";");
+        }
+        if(diameter!=0)
+            addTree(tree,treeName,diameter);
+        addTreeHood(hood, hoodName);
+    }
+        hoodList(hood);
+        vecToList(tree);
+        toBegin(tree);
+        toBeginHoodHab(hood);
+        toBeginQty(hood);
+        int qty;
 
-    //     while(hasNextHoodQty(hood)){
-    //         nextHoodQty(hood, &qty, hoodName);
-    //         //fprintf(query1,"%s\n",prueba);
-    //         fprintf(query1,"%s;%d\n",hoodName,qty);
-    //     }
+        while(hasNextHoodQty(hood)){
+            nextHoodQty(hood, &qty, hoodName);
+            fprintf(query1,"%s;%d\n",hoodName,qty);
+        }
       
-    //     //query 1 es arboles por barrio - necesito barrios y cant de arboles 
-    //     //query 2 es total de arboles por habitante - necesito barrios y arboles por hab
-    //     //query 3 es diametro promedio por especie de arbol - necesito nombre del arbol y promedio del diametro 
+        //query 1 es arboles por barrio - necesito barrios y cant de arboles 
+        //query 2 es total de arboles por habitante - necesito barrios y arboles por hab
+        //query 3 es diametro promedio por especie de arbol - necesito nombre del arbol y promedio del diametro 
 
-    //     char name[100];
-    //     while(hasNext(tree)){
-    //         diameter=next(tree,name);
-    //         fprintf(query3,"%s;%f\n",name,diameter);
-    //     }
-
-    
-    
+        char name[100];
+        while(hasNext(tree)){
+            diameter=next(tree,name);
+            fprintf(query3,"%s;%f\n",name,diameter);
+        }
+    freeHood(hood);
+    freeTree(tree);
 }
