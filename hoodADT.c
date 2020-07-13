07105a002b8084152ea987532540a777618be5ae
federojas <frojas@itba.edu.ar>
2020-07-13 14:08:18 -0300

    a

--- hoodADT.c
+++ hoodADT.c
@@ -90,7 +90,7 @@ static void treesHab(hoodADT hood)
     for(int i = 0; i < hood->vecSize; i++) {
         habitants = hood->vecHood[i].habitants;
         trees = hood->vecHood[i].treeQty;
-        float treesHab = __floorf( ((double)trees/(double)habitants) * 100) / 100;
+        float treesHab = floorf( ((double)trees/(double)habitants) * 100) / 100;
         hood->vecHood[i].treesPerHab = treesHab;
     }
 }
